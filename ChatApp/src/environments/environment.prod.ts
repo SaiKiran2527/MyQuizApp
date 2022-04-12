791a58825ffd983e98d4b3f1626c149e9668b0ac
Sathvika Patwari <sathvika0123@gmail.com>
2022-04-13 00:26:29 +0530

    third commit

--- ChatApp/src/environments/environment.prod.ts
+++ ChatApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase : {
+    apiKey: "AIzaSyA8_FFTj0JtskxfdiVGwUSlg6uheVVFdxI",
+    authDomain: "angularchatapp-44a33.firebaseapp.com",
+    projectId: "angularchatapp-44a33",
+    storageBucket: "angularchatapp-44a33.appspot.com",
+    messagingSenderId: "518981224031",
+    appId: "1:518981224031:web:636da1d67c3d6a5d1afdea",
+    measurementId: "G-L29VXGH29L"
+  }
 };
